ce26b39a2bca20d83beaa17ed791e85adab6cd80
egraille <58520630+egraille@users.noreply.github.com>
2020-01-30 17:36:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $lc = "";
 // Check to see that the global language server variable isset()
 // If it is set, we cut the first two characters from that string
 if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
-$lc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+$lc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0,1);
 
 // Now we simply evaluate that variable to detect specific languages
 if($lc == "fr"){
